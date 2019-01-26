a6d6585f3793914cc6da29ccc508451ae4c08a4a
JackAlgera <jack.algera@live.com>
2019-01-26 23:16:16 +0100

    Merge

---
+++ Assets/Scripts/Controllers/DiamondController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondController : MonoBehaviour
+{
+    public ParticleSystem partSys;
+
+    private void Start()
+    {
+        PlayerController.OnItemCarried += OnItemCarried;
+        PlayerController.OnItemDropped += OnItemDropped;
+    }
+
+    private void OnDestroy()
+    {
+        PlayerController.OnItemCarried -= OnItemCarried;
+        PlayerController.OnItemDropped -= OnItemDropped;
+    }
+
+    private void OnItemDropped(GameObject item)
+    {
+        partSys.Stop();
+    }
+
+    private void OnItemCarried(GameObject item)
+    {
+        partSys.Play();
+    }
+}
