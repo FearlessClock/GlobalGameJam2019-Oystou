4d024196c932a1ec62f70226f18cd27457744b20
JackAlgera <jack.algera@live.com>
2019-01-26 18:04:26 +0100

    Quick fix

---
+++ Assets/SpeechBubbleController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubbleController : MonoBehaviour
+{
+    
+    public SpeechBubble bubbleObject;
+
+    void Start()
+    {
+        transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = bubbleObject.objectPhoto;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
