6d737398baf9938db140589ea168a7b4ea4e2d3a
Pieter Algera <pieteralgera@ymail.com>
2019-01-26 11:59:22 +0100

    Added powerline that spawns between 2 points

--- Assets/ParticleLinePlacer.cs
+++ Assets/ParticleLinePlacer.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ParticleLinePlacer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform point1;
+    public Transform point2;
+    public float lineSizeOffset;
     void Start()
     {
-        
-    }
+        float distanceBetweenPoints = (point1.position - point2.position).magnitude;
+        float ratio = distanceBetweenPoints / this.transform.localScale.magnitude - lineSizeOffset;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 direction = (point2.position - point1.position).normalized;
+        float angle = Vector3.SignedAngle(direction, this.transform.rotation * Vector3.right, -Vector3.forward);
+        this.transform.localScale = Vector3.one * ratio;
+        this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        this.transform.position = point1.position + direction * distanceBetweenPoints / 2;
+
     }
 }
