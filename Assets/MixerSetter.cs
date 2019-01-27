3f677a5b51b4ea49ecb6e26eb8b108edb3026cd8
Pieter Algera <pieteralgera@ymail.com>
2019-01-27 01:59:52 +0100

    Added main menu and sound settings

---
+++ Assets/MixerSetter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixerSetter : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void ToggleMute()
+    {
+        float masterVol = 0;
+        mixer.GetFloat("MasterVol", out masterVol);
+        if (masterVol < 0)
+        {
+            mixer.SetFloat("MasterVol", 0);
+        }
+        else
+        {
+            mixer.SetFloat("MasterVol", -80);
+        }
+    }
+}
